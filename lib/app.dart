94666b8d42b4ba99621bbc5fe529efd7d886045b
hpodong <hjangwoo@gmail.com>
2025-03-19 15:16:48 +0900

    push

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'customs/custom.dart';
 import 'providers/router_provider.dart';
@@ -7,6 +8,10 @@ class App extends ConsumerWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // 상태바 배경색 (흰색으로 설정)
+      statusBarIconBrightness: Brightness.dark, // 상태바 아이콘 색상 (dark/light)
+    ));
     final router = ref.watch(routerProvider);
     return MaterialApp.router(
       theme: ThemeData(
