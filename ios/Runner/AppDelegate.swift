7fdbf8507e9ab94db33df26bcd7acd1165debcab
hpodong <hpodong@naver.com>
2026-04-18 17:51:14 +0900

    feat:push

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,8 @@
 import UIKit
 import Flutter
-import FirebaseCore
-import NaverThirdPartyLogin
+import Firebase
 import app_links
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,6 +13,14 @@ import app_links
 
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
+
+      FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+          GeneratedPluginRegistrant.register(with: registry)
+      }
+
+      if #available(iOS 10.0, *) {
+          UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      }
 
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
     let LAUNCH_CHANNEL = "method_channel"
@@ -60,17 +68,6 @@ import app_links
     }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-      // 네이버 로그인 URL 처리
-      if NaverThirdPartyLoginConnection.getSharedInstance().application(app, open: url, options: options) {
-          return true
-      }
-
-      // 기타 앱에서 처리해야 할 URL을 여기에 추가
-
-      return super.application(app, open: url, options: options)
   }
 }
 
